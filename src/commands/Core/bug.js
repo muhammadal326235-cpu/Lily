ab6ec2e07cc34bbaeabe16853909b5b0b24f5dab
Annoyying user <muhammad.al326235@smp.belajar.id>
2026-06-01 23:04:16 +0800

    Update bug.js

--- src/commands/Core/bug.js
+++ src/commands/Core/bug.js
@@ -11,7 +11,7 @@ export default {
         const githubButton = new ButtonBuilder()
             .setLabel('?? Report Bug on GitHub')
             .setStyle(ButtonStyle.Link)
-            .setURL('https://github.com/codebymitch/TitanBot/issues');
+            .setURL('https://github.com/muhammadal326235-cpu/Issues/issues/1#issue-4564138806');
 
         const row = new ActionRowBuilder().addComponents(githubButton);
 
@@ -19,10 +19,10 @@ export default {
             title: '?? Bug Report',
             description: 'Found a bug? Please report it on our GitHub Issues page!\n\n' +
             '**When reporting a bug, please include:**\n' +
-            '• ?? Detailed description of the issue\n' +
-            '• ?? Steps to reproduce the problem\n' +
-            '• ?? Screenshots if applicable\n' +
-            '• ?? Your bot version and environment\n\n' +
+            'â€¢ ?? Detailed description of the issue\n' +
+            'â€¢ ?? Steps to reproduce the problem\n' +
+            'â€¢ ?? Screenshots if applicable\n' +
+            'â€¢ ?? Your bot version and environment\n\n' +
             'This helps us fix issues faster and more effectively!',
             color: 'error'
         })
